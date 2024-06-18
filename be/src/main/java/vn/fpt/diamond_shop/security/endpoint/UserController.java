42c576ed5b119d6b43dc74d0faa5512452080f96
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-06-18 21:28:53 +0700

    Nhat - Preparation for new endpoints

--- be/src/main/java/vn/fpt/diamond_shop/security/endpoint/UserController.java
+++ be/src/main/java/vn/fpt/diamond_shop/security/endpoint/UserController.java
@@ -1,37 +1,10 @@
 package vn.fpt.diamond_shop.security.endpoint;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.fpt.diamond_shop.controller.BaseController;
-import vn.fpt.diamond_shop.request.ChangeProfileRequest;
-import vn.fpt.diamond_shop.security.AccountService;
-import vn.fpt.diamond_shop.security.exception.ResourceNotFoundException;
-import vn.fpt.diamond_shop.security.model.User;
-import vn.fpt.diamond_shop.repository.UserRepository;
-import vn.fpt.diamond_shop.security.CurrentUser;
-import vn.fpt.diamond_shop.security.UserPrincipal;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/shop/user")
 public class UserController extends BaseController {
-    @Autowired
-    private UserRepository userRepository;
 
-    @Autowired
-    private AccountService accountService;
-
-    @GetMapping("/me")
-    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
-        return userRepository.findById(userPrincipal.getId()).orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
-    }
-
-    @PostMapping("/change-profile")
-    public ResponseEntity<?> changeProfile(@CurrentUser UserPrincipal userPrincipal, @RequestBody ChangeProfileRequest changeProfileRequest) {
-        User user = userRepository.findById(userPrincipal.getId()).orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
-
-        changeProfileRequest.setEmail(user.getEmail());
-        accountService.changeProfile(changeProfileRequest);
-        return ok("Change profile successfully");
-    }
 }
