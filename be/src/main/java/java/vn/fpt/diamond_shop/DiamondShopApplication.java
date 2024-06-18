03decba41dcaa0ec742c6068e6068c4f9311c28f
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-06-18 19:37:01 +0700

    Nhat - Updated BE structure

---
+++ be/src/main/java/java/vn/fpt/diamond_shop/DiamondShopApplication.java
@@ -0,0 +1,13 @@
+package java.vn.fpt.diamond_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiamondShopApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/java/vn/fpt/diamond_shop/DiamondShopApplicationTests.java
@@ -0,0 +1,13 @@
+package java.vn.fpt.diamond_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
