befc3409110f818b5eb87ebcda6288c054fb1037
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-06-18 20:11:42 +0700

    Nhat - Update BE structure again

--- be/src/main/java/vn/fpt/diamond_shop/SpringSocialApplication.java
+++ be/src/main/java/vn/fpt/diamond_shop/SpringSocialApplication.java
@@ -1,13 +1,13 @@
-package java.vn.fpt.diamond_shop;
+package vn.fpt.diamond_shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiamondShopApplication {
+public class SpringSocialApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiamondShopApplication.class, args);
+        SpringApplication.run(SpringSocialApplication.class, args);
     }
 
 }
