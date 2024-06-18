f9685d1ba53fec38c04932f5e1aa8e393fd80a14
Nguyen Trung Viet Anh <115200119+Vietanh2703@users.noreply.github.com>
2024-06-19 00:10:19 +0700

    First commit !

---
+++ src/main/java/com/anhntv/diamondshop/DiamondshopApplication.java
@@ -0,0 +1,13 @@
+package com.anhntv.diamondshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anhntv/diamondshop/DiamondshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhntv.diamondshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
