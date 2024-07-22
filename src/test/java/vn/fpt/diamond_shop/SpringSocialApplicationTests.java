13eb789bd20b84d089f086fe9e16b132db3737bf
Trần Đăng Khoa <khoatdse172986@fpt.edu.vn>
2024-07-22 14:25:38 +0700

    0.14

--- src/test/java/vn/fpt/diamond_shop/SpringSocialApplicationTests.java
+++ src/test/java/vn/fpt/diamond_shop/SpringSocialApplicationTests.java
@@ -1,13 +1,13 @@
-package com.anhntv.diamondshop;
+package vn.fpt.diamond_shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiamondshopApplicationTests {
+public class SpringSocialApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
