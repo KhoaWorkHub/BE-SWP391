ab88a0388d99153252f5386d0801e31a514cf1b5
Trần Đăng Khoa <khoatdse172986@fpt.edu.vn>
2024-07-25 00:02:43 +0700

    order_detailed_fix

--- src/main/java/vn/fpt/diamond_shop/service/Impl/OrderServiceImpl.java
+++ src/main/java/vn/fpt/diamond_shop/service/Impl/OrderServiceImpl.java
@@ -263,7 +263,8 @@ public class OrderServiceImpl implements OrderService {
         Orders order = ordersRepository.findById(request.getOrderId()).orElseThrow(() -> new DiamondShopException(400, "Order not found"));
 
         List<OrdersListAllUser> ordersListAllUsers = new ArrayList<>();
-        
+        OrdersListAllUser ordersListAllUser = new OrdersListAllUser();
+        BeanUtils.copyProperties(order, ordersListAllUser);
         List<OrderDetail> allByUniqueOrderId = orderDetailRepository.findAllByUniqueOrderId(order.getUniqueOrderId());
         ordersListAllUser.setOrderDetails(allByUniqueOrderId);
         ordersListAllUser.setDeliveryInfo(deliveryRepository.findAllByOrderId(order.getUniqueOrderId()));
